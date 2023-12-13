71806c3116b25109bd44e2118cdb259ebf64f5d4
Swadhin941 <swadhinghose@gmail.com>
2023-12-13 20:50:17 +0600

    profile update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 const jwt = require('jsonwebtoken');
@@ -49,8 +49,23 @@ const forbiddenAccess = (req, res, next) => {
 
 const run = async () => {
     const Users = client.db("Elegant").collection("Users");
-
+    const shopRequest = client.db("Elegant").collection("ShopRequest");
+    const AllShop = client.db("Elegant").collection("AllShop");
+    const AllProducts= client.db("Elegant").collection("AllProducts");
     try {
+
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const findEmail = await Users.findOne({ email: email });
+            if (findEmail) {
+                if (findEmail.role === 'admin') {
+                    next();
+                }
+                else {
+                    return res.status(401).send({ message: "Un-authorize Access" });
+                }
+            }
+        }
 
         app.post('/user', async (req, res) => {
             const userData = req.body;
@@ -68,13 +83,23 @@ const run = async () => {
             const email = req.decoded.email;
             let getEmailInfo = await Users.findOne({ email });
             if (getEmailInfo) {
+                const getAvailable = await AllShop.countDocuments({ owner: req.decoded.email });
+                getEmailInfo.totalShop = getAvailable;
                 return res.send(getEmailInfo);
             }
         });
 
+        app.get('/adminCheck', async (req, res) => {
+            const email = req.query.user;
+            const findEmail = await Users.findOne({ email: email });
+            if (findEmail) {
+                return res.send({ admin: findEmail?.role === 'admin' });
+            }
+        })
+
         app.post('/jwt', (req, res) => {
             const email = req.body.email;
-            const token = jwt.sign({ email }, ACCESS_TOKEN, { expiresIn: "5h" });
+            const token = jwt.sign({ email }, ACCESS_TOKEN, { expiresIn: "1h" });
             res.send({ token });
         })
 
@@ -115,6 +140,45 @@ const run = async () => {
             res.send(result);
         })
 
+        app.post('/createShop', verifyJWT, forbiddenAccess, async (req, res) => {
+            const result = await shopRequest.insertOne({ ...req.body, owner: req.decoded.email });
+            res.send(result);
+        })
+
+        app.get('/shopRequest', verifyJWT, forbiddenAccess, async (req, res) => {
+            const result = await shopRequest.find({ approved: { $ne: true } }).toArray();
+            res.send(result);
+        })
+
+        app.put('/shopApproved', verifyJWT, verifyAdmin, forbiddenAccess, async (req, res) => {
+            console.log(req.body);
+            const insertShop = await AllShop.insertOne(req.body);
+            const filter = { _id: new ObjectId(req.body._id) };
+            const updatedDoc = {
+                $set: {
+                    approved: true
+                }
+            };
+            const option = { upsert: true };
+            const result = await shopRequest.updateOne(filter, updatedDoc, option);
+            res.send(result);
+        })
+
+        app.put('/shopRejected', verifyJWT, verifyAdmin, forbiddenAccess, async (req, res) => {
+            console.log('166', req.body);
+        })
+
+        app.get('/dashboard/add-product/warehouse', verifyJWT, forbiddenAccess, async (req, res) => {
+            const email = req.decoded.email;;
+            const findWarehouse = await AllShop.findOne({ owner: email });
+            res.send({ warehouse: findWarehouse?.warehouse });
+        })
+
+        app.post('/dashboard/add-product', verifyJWT, forbiddenAccess, async(req, res)=>{
+            console.log(req.body);
+            const result = await AllProducts.insertOne({...req.body});
+            res.send(result)
+        })
 
         app.get('/', (req, res) => {
             res.send("Elegant server work");
